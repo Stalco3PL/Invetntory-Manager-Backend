cbf2a7e74b4586c5c03c7af3cc901f83facb671f
RayHan904 <54216177+RayHan904@users.noreply.github.com>
2024-04-21 14:39:26 -0400

    fixed UI

--- src/screens/CustomerDashboard.tsx
+++ src/screens/CustomerDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, ListGroup, InputGroup, FormControl } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, InputGroup, FormControl, Card } from 'react-bootstrap';
 import { BsX } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -59,33 +59,51 @@ const CustomerDashboard: React.FC = () => {
     let whlData = 0;
 
     const barChartData = {
-            data: [claysonCount, whlCount],
-      };
+        data: [claysonCount, whlCount],
+    };
     return (
-        <Container fluid style={{ minHeight: '85vh'}}>
+        <Container fluid style={{ minHeight: '85vh' }}>
             <Row style={{ height: '30vh', overflowY: 'auto' }}>
                 <h1>{selectedCustomer?.companyName}</h1>
                 {isLoading ? (
                     <Loader />
                 ) : (
                     <>
-                        <Col>
-                            <h4>Total Inventory: {summary?.Total}</h4>
-                            <h4>WHL total: {whlCount}</h4>
-                            <h4>Clayson total: {claysonCount}</h4>
+                        <Col md={6} sm={3}>
+                            <Card className="text-center" style={{ width: '50%', margin: '0 auto', marginBottom: '10px', padding: '3px' }}>
+                                <Card.Body style={{ padding: '0' }}>
+                                    <Card.Title>Total Inventory</Card.Title>
+                                    <Card.Text> {typeof summary?.Total === 'number' ? summary?.Total.toLocaleString() : 'N/A'} </Card.Text>
+                                </Card.Body>
+                            </Card>
+
+                            <Card className="text-center" style={{ width: '50%', margin: '0 auto', marginBottom: '10px', padding: '3px' }}>
+                                <Card.Body style={{ padding: '0' }}>
+                                    <Card.Title>WHL Total</Card.Title>
+                                    <Card.Text> {typeof whlCount === 'number' ? whlCount.toLocaleString() : 'N/A'}  {summary && summary.Total ? `(${((whlCount / summary.Total) * 100).toFixed(2)}%)` : 'N/A'}</Card.Text>
+                                </Card.Body>
+                            </Card>
+
+                            <Card className="text-center" style={{ width: '50%', margin: '0 auto', padding: '3px' }}>
+                                <Card.Body style={{ padding: '0' }}>
+                                    <Card.Title>Clayson Total</Card.Title>
+                                    <Card.Text> {typeof claysonCount === 'number' ? claysonCount.toLocaleString() : 'N/A'} {summary && summary.Total ? `(${((claysonCount / summary.Total) * 100).toFixed(2)}%)` : 'N/A'}</Card.Text>
+                                </Card.Body>
+                            </Card>
                         </Col>
-                        <Col>
-                        <div style={{
-                                width: '100%',
-                                height: '24vh', // Set the height as per your requirement
+                        <Col md={6} >
+                            <div style={{
+                                maxHeight: '24vh', // Set the height as per your requirement
                             }}>
                                 <BarChartComponent barChartData={barChartData} />
                             </div>
                         </Col>
+
+
                     </>
                 )}
             </Row>
-            <hr/>
+            <hr />
             <Row>
                 <Col lg={3} md={4} sm={6} xs={12} style={{ maxHeight: '70vh', overflowY: 'auto' }}>
                     <h2>SKUs</h2>
@@ -101,11 +119,11 @@ const CustomerDashboard: React.FC = () => {
                     </InputGroup>
                     {isLoading ? (
                         <div className="m-5" >
-                        <Loader  />
+                            <Loader />
 
                         </div>
                     ) : (
-                        
+
                         <ListGroup style={{ height: window.innerWidth <= 768 ? '30vh' : '100%', overflowY: 'auto' }} >
                             {filteredItems.map(item => (
                                 <ListGroup.Item
@@ -121,49 +139,44 @@ const CustomerDashboard: React.FC = () => {
                     )}
 
                 </Col>
-                {window.innerWidth <= 768 ? <hr/> : <></>}
+                {window.innerWidth <= 768 ? <hr /> : <></>}
 
                 <Col lg={9} md={8} sm={6} xs={12}>
-    <div>
-        {selectedItem ? (
-            <div>
-                <h2>{selectedItem}</h2>
-                <ul>
-                    {Object.entries(details[selectedItem] || {}).map(([key, value]) => {
-                        if (key === "Clayson") {
-                            claysonData = value;
-                        } else {
-                            whlData = value;
-                        }
-                        return (
-                            <li key={key}>
-                                {key}: {value}
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        ) : (
-            <p>Select an item to view details</p>
-        )}
-        {selectedItem && (
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '100%',
-                height: '50vh'
-            }}>
-                <PieChartComponent pieChartData={{ data: [claysonData, whlData] }} />
-            </div>
-        )}
-    </div>
-</Col>
-
-
-
-
-
+                    <div>
+                        {selectedItem ? (
+                            <div>
+                                <h2>{selectedItem}</h2>
+                                <ListGroup style={{ width:  window.innerWidth <= 768 ? '50%' : '30%' }}>
+                                    {Object.entries(details[selectedItem] || {}).map(([key, value]) => {
+                                        if (key === "Clayson") {
+                                            claysonData = value;
+                                        } else {
+                                            whlData = value;
+                                        }
+                                        return (
+                                            <ListGroup.Item key={key}>
+                                                {key}: {value}
+                                            </ListGroup.Item>
+                                        );
+                                    })}
+                                </ListGroup>
+                            </div>
+                        ) : (
+                            <p>Select an item to view details</p>
+                        )}
+                        {selectedItem && (
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                width: '100%',
+                                height: '50vh'
+                            }}>
+                                <PieChartComponent pieChartData={{ data: [claysonData, whlData] }} />
+                            </div>
+                        )}
+                    </div>
+                </Col>
             </Row>
         </Container>
     );
